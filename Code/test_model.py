d90ee0bf753bd689614f56bdbde9fe40a5c9d5ed
Pha <phakaewpra@gmail.com>
2026-01-05 00:49:55 +0700

    add keyboard and find dist

--- Code/test_model.py
+++ Code/test_model.py
@@ -2,33 +2,77 @@ import cv2
 from ultralytics import YOLO
 import mss
 import numpy as np
+import keyboard
+from pynput.mouse import Controller
 
-monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+#--------Settings----------
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+CENTER_X  = SCREEN_WIDTH // 2
+CENTER_Y = SCREEN_HEIGHT // 2
+
+#   Detection Zone
+#   FOV box size
+FOV_SIZE = 400 
+monitor = {
+    "top": CENTER_Y - FOV_SIZE // 2, 
+    "left": CENTER_X - FOV_SIZE // 2, 
+    "width": FOV_SIZE, 
+    "height": FOV_SIZE
+}
+
+AIM_SPEED = 0.4
+
+print("Loading YOLO Model.....")
 model = YOLO("../Model/best1.pt")
+mouse = Controller()
+
+print("Ready! Hold 'p' to track a BAT!!")
 
 with mss.mss() as sct:
     while True:
+        # --- Capture & Preprocess ---
         img = sct.grab(monitor)
         frame = np.array(img)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
         
+        # --- Inference ---
         results = model(frame, conf=0.5, verbose=False)
-        
-        # 1. Get the annotated frame (optional, if you still want the boxes)
         annotated_frame = results[0].plot()
         
-        # 2. Iterate through detected boxes to find centers
+        # --- Logic: Find Closest Target ---
+        closest_dist = float('inf')
+        target_offset = None
+        
+        
         for result in results:
-            boxes = result.boxes.xyxy  # Get boxes in [x1, y1, x2, y2] format
-            
-            for box in boxes:
-                # Calculate center coordinates
+            for box in result.boxes.xyxy:
                 x1, y1, x2, y2 = box
-                center_x = int((x1 + x2) / 2)
-                center_y = int((y1 + y2) / 2)
+                # --- Find Center Box ---
+                box_center_x = int((x1 + x2) / 2)
+                box_center_y = int((y1 + y2) / 2)
                 
-                # 3. Draw the center point
-                # cv2.circle(image, center_coordinates, radius, color, thickness)
+                # --- ABS Screen Coordiantes ---
+                abs_x = box_center_x + monitor["left"]
+                abs_y = box_center_y + monitor["top"]
+                
+                # --- Calculate Distance From Crosshair
+                dist_x = abs_x - CENTER_X
+                dist_y = abs_y - CENTER_Y
+                
+                # --- Distance From Crosshair
+                current_dist = (dist_x ** 2 + dist_y ** 2 ) ** 0.5
+                
+                # --- Keep only the closest to the crosshair
+                if current_dist < closest_dist:
+                    closest_dist = current_dist
+                    target_offset = (dist_x, dist_y)
+                
+        # --- Control: Move Mouse ----
+        if keyboard.is_pressed('p') and 
+                
+                
                 cv2.circle(annotated_frame, (center_x, center_y), 5, (0, 0, 255), -1) 
                 
                 # Optional: Draw a label for the coordinates
